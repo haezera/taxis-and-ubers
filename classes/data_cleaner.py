fd8d573c7e212509744dd6122b6c0c2ff22a5a66
haeohreumkim <z5480978@ad.unsw.edu.au>
2025-03-21 09:31:24 +1100

    NEW: 	- Data cleaning tool has started to be written 	- Next is research on how locations work in the data 	- Then we can formally start piecing together some descriptive statistics, 	and other things we might want

--- classes/data_cleaner.py
+++ classes/data_cleaner.py
@@ -1,1 +1,44 @@
-import pandas
+import pandas as pd
+import numpy as np
+
+class TaxiDataCleaner:
+    def __init__(self, path: str):
+        self.data = pd.read_parquet(path)
+        self.path = path
+        self.quant_fields = [
+            'passenger_count', 'trip_distance', 
+            'fare_amount', 
+            'tip_amount', 
+            'tolls_amount', 
+            'total_amount'
+        ]
+
+    def remove_nans(self):
+        """
+        Removes any rows that have NaNs in our quantitative fields
+        """
+        self.data = self.data.dropna(subset=self.quant_fields)
+
+    def remove_nonsense_values(self):
+        """
+        In our data quality analysis, we defined the bounds for our analysis.
+        This means we want:
+            - No trips of 0 distance 
+            - We want taxi trips that only plausibly occured within NYC 
+            - We want trips of atleast 0.1 miles
+            - We want a maximum of 5 passengers.
+        """
+        self.data = self.data[
+            (self.data['trip_distance'] != 0) &     # we can't have trips of 0 distance
+            (self.data['trip_distance'] < 35) &     # NYC is 35 miles wide at most
+            (self.data['trip_distance'] >= 0.1) &   # atleast 0.1 miles
+            (self.data['passenger_count'] <= 5)     # biggest taxi is a mini van of 5
+        ]
+
+    def remove_negative_values(self):
+        self.data = self.data[
+            (self.data[self.quant_fields] > 0).all(axis=1)
+        ]
+
+    def write_data(self, path: str):
+        self.data.to_parquet(path)
